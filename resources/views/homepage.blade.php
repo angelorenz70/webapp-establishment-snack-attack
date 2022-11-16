54cb8283e217c8d42fbd5d9a27475467ad750fd2
breech13 <breechekay@gmail.com>
2022-11-16 15:41:26 +0800

    changing logo of each service and name

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -18,27 +18,24 @@
                 </div>
                 <div class="row text-center">
                     <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        <span class="fa-stack fa-4x mb-4">
+                            <img class="img-fluid" src="assets-homepage/assets/img/serviceslogo/services1.png" alt="..." />
                         </span>
                         <h4 class="my-3">E-Commerce</h4>
                         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
                     </div>
                     <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                        <span class="fa-stack fa-4x mb-4">
+                            <img class="img-fluid" src="assets-homepage/assets/img/serviceslogo/services2.png" alt="..." />
                         </span>
-                        <h4 class="my-3">Responsive Design</h4>
+                        <h4 class="my-3">Walk-in</h4>
                         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
                     </div>
                     <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                        <span class="fa-stack fa-4x mb-4">
+                            <img class="img-fluid" src="assets-homepage/assets/img/serviceslogo/services3.png" alt="..." />
                         </span>
-                        <h4 class="my-3">Web Security</h4>
+                        <h4 class="my-3">Booking</h4>
                         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
                     </div>
                 </div>
