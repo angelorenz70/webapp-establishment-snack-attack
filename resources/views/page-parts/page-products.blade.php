6a8175a644e5c77cbbc89f2ef4570ec8819745c9
breech13 <breechekay@gmail.com>
2022-12-28 20:17:34 +0800

    addding new folders

--- resources/views/page-parts/page-products.blade.php
+++ resources/views/page-parts/page-products.blade.php
@@ -6,18 +6,18 @@
             <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="col-lg-3 col-sm-6 mb-4">
                 <!-- Portfolio item 1-->
                 <div class="portfolio-item">
                     <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal1">
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dish1.png" alt="..." />
+                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dis1.jpg" alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">product1</div>
-                        <div class="portfolio-caption-subheading text-muted">Fried Rice</div>
+                        <div class="portfolio-caption-subheading text-muted">Halo-halo</div>
                     </div>
                 </div>
             </div>
@@ -28,26 +28,26 @@
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dish2.png" alt="..." />
+                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dis2.jpg" alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">product2</div>
-                        <div class="portfolio-caption-subheading text-muted">Ramen</div>
+                        <div class="portfolio-caption-subheading text-muted">noodles</div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="col-lg-3 col-sm-6 mb-3">
                 <!-- Portfolio item 3-->
                 <div class="portfolio-item">
                     <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal3">
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dish3.png" alt="..." />
+                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dis3.jpg" alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">product3</div>
-                        <div class="portfolio-caption-subheading text-muted">Salad</div>
+                        <div class="portfolio-caption-subheading text-muted">Ham&Egg Sandwhich</div>
                     </div>
                 </div>
             </div>
@@ -58,11 +58,11 @@
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dish4.png" alt="..." />
+                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dis4.jpg" alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">product4</div>
-                        <div class="portfolio-caption-subheading text-muted">Spicy Ramen</div>
+                        <div class="portfolio-caption-subheading text-muted">Milk Tea</div>
                     </div>
                 </div>
             </div>
@@ -73,11 +73,11 @@
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dish5.png" alt="..." />
+                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dis5.jpg" alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">product5</div>
-                        <div class="portfolio-caption-subheading text-muted">Burger</div>
+                        <div class="portfolio-caption-subheading text-muted">Pizza</div>
                     </div>
                 </div>
             </div>
@@ -88,11 +88,11 @@
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dish6.png" alt="..." />
+                        <img class="img-fluid" src="assets-homepage/assets/img/portfolio/dis6.jpg" alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">product6</div>
-                        <div class="portfolio-caption-subheading text-muted">Lomi</div>
+                        <div class="portfolio-caption-subheading text-muted">Mango Graham</div>
                     </div>
                 </div>
             </div>
