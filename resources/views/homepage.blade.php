d31a4175e995c181c9c12645ff46003fea5ac235
breech13 <breechekay@gmail.com>
2022-12-29 11:59:43 +0800

    replacing product3

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -121,8 +121,8 @@
                             <div class="col-lg-8">
                                 <div class="modal-body">
                                     <!-- Project details-->
-                                    <h2 class="text-uppercase">Project Name</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                                    <h2 class="text-uppercase">Ham&Egg Sandwhich</h2>
+                                    <p class="item-intro text-muted">Taste, Enjoy, Remember!</p>
                                     <img class="img-fluid d-block mx-auto" src="assets-homepage/assets/img/portfolio/dis3.jpg" alt="..." />
                                     <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
                                     <ul class="list-inline">
