27239f467eea52482a79fc26d71517590f9cc8d1
breech13 <breechekay@gmail.com>
2023-01-01 13:29:12 +0800

    changing some information

--- resources/views/page-parts/page-owner.blade.php
+++ resources/views/page-parts/page-owner.blade.php
@@ -3,14 +3,14 @@
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">Owner</h2>
-            <h3 class="section-subheading text-muted">Enjoy, Taste, Remember!</h3>
+            <h3 class="section-subheading text-muted"></h3>
         </div>
         <div class="row">
             <div class="col-lg-12">
                 <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="assets-homepage/assets/img/team/pic.jpg" alt="..." />
+                    <img class="mx-auto rounded-circle" src="assets-homepage/assets/img/team/pic123.jpg" alt="..." />
                     <h4>Joann Suson Plaza</h4>
-                    <p class="text-muted">FB: kimplaza joannsusonplaza </p>
+                    <p class="text-muted">Business Women </p>
                     <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i class="fab fa-facebook-f"></i></a>
                     <a class="btn bgitn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
