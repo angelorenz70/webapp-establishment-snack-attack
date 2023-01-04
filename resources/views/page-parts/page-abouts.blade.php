17297ddcb8cebff2d7f88ee649b42719beb5f3d1
breech13 <breechekay@gmail.com>
2023-01-04 23:27:15 +0800

    changing some information in about us page/blade

--- resources/views/page-parts/page-abouts.blade.php
+++ resources/views/page-parts/page-abouts.blade.php
@@ -3,27 +3,27 @@
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">About</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h3 class="section-subheading text-muted">Taste, Enjoy, Remember!.</h3>
         </div>
         <ul class="timeline">
             <li>
                 <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets-homepage/assets/img/about/1.jpg" alt="..." /></div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
-                        <h4>2009-2011</h4>
-                        <h4 class="subheading">Our Humble Beginnings</h4>
+                        <h4>1998 - 2023</h4>
+                        <h4 class="subheading">Served Every Day Since 1998</h4>
                     </div>
-                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                    <div class="timeline-body"><p class="text-muted">Snack Attack opened on  Christmass Day 1998 business women/owner Joann Suson Plaza began making halo halo and pizza and selling theme to restaurant and his neighbors out of a small  kitchen at  the corner of Ochoa Avenue beside Breadwalk Bakeshop.!</p></div>
                 </div>
             </li>
             <li class="timeline-inverted">
                 <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets-homepage/assets/img/about/2.jpg" alt="..." /></div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
-                        <h4>March 2011</h4>
-                        <h4 class="subheading">An Agency is Born</h4>
+                        <h4>1998</h4>
+                        <h4 class="subheading">Modern Restaurant</h4>
                     </div>
-                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                    <div class="timeline-body"><p class="text-muted">A modern resturant with a menu that will make your mouth water. Serving delicious food since 45 years. Enjoy our seasonal menu and experience the beauty of naturalness!</p></div>
                 </div>
             </li>
             <li>
