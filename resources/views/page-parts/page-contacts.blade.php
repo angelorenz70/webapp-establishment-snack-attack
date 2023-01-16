397d11f83614b9d22f8fd709fcb6c2e14a4cbbe9
breech13 <breechekay@gmail.com>
2023-01-16 09:47:49 +0800

    changing  the contact us part

--- resources/views/page-parts/page-contacts.blade.php
+++ resources/views/page-parts/page-contacts.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase" data-aos="zoom-in" data-aos-delay="100">Contact Us</h2>
-            <h3 class="section-subheading text-muted" data-aos="fade-up" data-aos-delay="150">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h3 class="section-subheading text-muted" data-aos="fade-up" data-aos-delay="150">Have some question?</h3>
         </div>
         <!-- * * * * * * * * * * * * * * *-->
         <!-- * * SB Forms Contact Form * *-->
